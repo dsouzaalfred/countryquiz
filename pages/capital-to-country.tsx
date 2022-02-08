ee792fa3c98b766eebf5c7b6720bcae1f5fb143a
Alfred Dsouza <dsouzaalfred@gmail.com>
2022-02-08 10:17:58 +0000

    add capital to country page

---
+++ pages/capital-to-country.tsx
@@ -0,0 +1,59 @@
+import type { NextPage } from "next";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+
+import { PageLayout } from "@/layouts/Page";
+import { QuestionCard } from "@/components/QuestionCard";
+import { PageTitle } from "@/components/PageTitle";
+
+import {
+  getRandomIndex,
+  constructFlagOptions,
+  shuffleArray,
+} from "@/utils/index";
+
+import data from "@/data/countries.json";
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const randomCountry = getRandomIndex(data.length);
+
+  const possibleAnswers: number[] = [
+    randomCountry,
+    getRandomIndex(data.length),
+    getRandomIndex(data.length),
+    getRandomIndex(data.length),
+  ];
+
+  const options = constructFlagOptions(possibleAnswers, data);
+
+  shuffleArray(options);
+
+  return {
+    props: {
+      answerObject: data[randomCountry],
+      options: options,
+      questionType: "capitalToCountry",
+      question: data[randomCountry].capital,
+    },
+  };
+};
+
+const CapitalToCountry: NextPage = ({
+  answerObject,
+  options,
+  questionType,
+  question,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  return (
+    <PageLayout>
+      <PageTitle title="This is the capital of?" />
+      <QuestionCard
+        answerObject={answerObject}
+        options={options}
+        questionType={questionType}
+        question={question}
+      />
+    </PageLayout>
+  );
+};
+
+export default CapitalToCountry;
