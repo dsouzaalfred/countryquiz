3ba305ce8a16f5d60c3cb0dad01301255247a896
Alfred Dsouza <dsouzaalfred@gmail.com>
2022-02-08 09:13:48 +0000

    Add questions component

---
+++ components/Question/index.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+interface QuestionProps {
+  questionType:
+    | "flagToCountry"
+    | "capitalToCountry"
+    | "currencyToCountry"
+    | "languageToCountry"
+    | "countryToFlag"
+    | "countryToCapital"
+    | "countryToCurrency"
+    | "countryToLanguage";
+  question: string;
+}
+
+export const Question = ({ questionType, question }: QuestionProps) => {
+  return (
+    <>
+      {questionType !== "flagToCountry" && (
+        <div className="w-full flex items-center justify-center">
+          <span className="font-bold text-3xl text-teal-800">{question}</span>
+        </div>
+      )}
+      {questionType === "flagToCountry" && (
+        <div className="w-full flex items-center justify-center">
+          <Image src={`/${question}.jpg`} alt="Flag" height={213} width={320} />
+        </div>
+      )}
+    </>
+  );
+};
