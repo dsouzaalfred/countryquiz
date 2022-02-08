47738f1f55ca34891651a11f3f67449058332eb9
Alfred Dsouza <dsouzaalfred@gmail.com>
2022-02-08 10:18:24 +0000

    add function to get capital options

--- utils/index.tsx
+++ utils/index.tsx
@@ -3,6 +3,13 @@ import { Options, Country } from "@/types/types";
 export const getRandomIndex = (length: number) =>
   Math.floor(Math.random() * length);
 
+export const shuffleArray = (array: Options[]) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+};
+
 export const constructFlagOptions = (
   options: number[],
   data: Country[]
@@ -14,9 +21,13 @@ export const constructFlagOptions = (
   return arr;
 };
 
-export const shuffleArray = (array: Options[]) => {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
+export const constructCapitalOptions = (
+  options: number[],
+  data: Country[]
+): Options[] => {
+  const arr = options.map((option) => {
+    const country = data[option];
+    return { label: country.capital, value: country.code };
+  });
+  return arr;
 };
