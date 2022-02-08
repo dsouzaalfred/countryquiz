c3beb11fcf40cf00382236fa1524cf406129e34b
Alfred Dsouza <dsouzaalfred@gmail.com>
2022-02-08 09:17:38 +0000

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,8 @@
 const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {
-      backgroundColor: ["label-checked"],
-    },
-  },
-  plugins: [
-    require("@tailwindcss/custom-forms"),
-    plugin(({ addVariant, e }) => {
-      addVariant("label-checked", ({ modifySelectors, separator }) => {
-        modifySelectors(({ className }) => {
-          const eClassName = e(`label-checked${separator}${className}`); // escape class
-          const yourSelector = 'input[type="radio"]'; // your input selector. Could be any
-          return `${yourSelector}:checked ~ .${eClassName}`; // ~ - CSS selector for siblings
-        });
-      });
-    }),
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
 };
