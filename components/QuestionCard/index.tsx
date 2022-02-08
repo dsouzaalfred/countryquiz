f48625ace78c7a4c9904c11153c7a93ba8f49fb3
Alfred Dsouza <dsouzaalfred@gmail.com>
2022-02-08 09:14:39 +0000

    use new components question card

--- components/QuestionCard/index.tsx
+++ components/QuestionCard/index.tsx
@@ -1,45 +1,36 @@
-import Image from "next/image";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
 import { Card } from "@/components/Card";
 
+import { Question } from "@/components/Question";
 import { QuestionsOptions } from "@/components/QuestionsOptions";
+import { Message } from "@/components/Message";
 
-interface Options {
-  label: string;
-  value: string;
-}
-
-interface QuestionCardProps {
-  flag: string;
-  capital?: string;
-  questionType?: "flag" | "capital";
-  options: Options[];
-}
+import { QuestionCardProps } from "@/types/types";
 
 export const QuestionCard = ({
-  flag,
-  capital,
+  answerObject,
   options,
-  questionType = "flag",
+  questionType,
+  question,
 }: QuestionCardProps) => {
   const [isShowAnswer, setIsShowAnswer] = useState<boolean>(false);
   const [isAnswerCorrect, setIsAnswerCorrect] = useState<boolean>(false);
+  const [selectedAnswer, setSelectedAnswer] = useState<string>("");
 
   const router = useRouter();
 
   const [isError, setIsError] = useState<boolean>(false);
 
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
+  const handleSubmit = (answer: string) => {
     setIsError(false);
-    const answer = e.target.answer.value;
     if (!answer) {
       setIsError(true);
       return;
     }
-    if (answer === flag) {
+    setSelectedAnswer(answer);
+    if (answer === answerObject.code) {
       setIsAnswerCorrect(true);
     } else {
       setIsAnswerCorrect(false);
@@ -51,69 +42,36 @@ export const QuestionCard = ({
     router.reload();
   };
 
-  const getCorrectAnswer = () => {
-    let answer = "";
-    options.forEach((el) => {
-      if (el.value === flag) {
-        answer = el.label;
-      }
-    });
-    return answer;
-  };
-
   return (
     <Card>
       <div className="flex flex-col w-full">
-        <div className="w-full flex items-center justify-center mb-4">
-          {questionType === "flag" && (
-            <p className="font-bold text-sky-600">Whose flag is it?</p>
-          )}
-          {questionType === "capital" && (
-            <p className="font-bold text-sky-600">Whose capital is it?</p>
-          )}
-        </div>
-        {questionType === "capital" && (
-          <div className="w-full flex items-center justify-center mb-4">
-            <span className="font-bold text-3xl text-teal-800">{capital}</span>
-          </div>
-        )}
-        {questionType === "flag" && (
-          <div className="w-full flex items-center justify-center mb-4">
-            <Image src={`/${flag}.jpg`} alt="Flag" height={213} width={320} />
-          </div>
-        )}
-        <form onSubmit={handleSubmit}>
+        <Question questionType={questionType} question={question} />
+        <>
           <div className="flex flex-col w-full p-8">
-            <QuestionsOptions questions={options} />
-            {isError && <p className="text-red-600">Please select an answer</p>}
-            {isShowAnswer && isAnswerCorrect && (
-              <p className="text-green-600">Correct!</p>
-            )}
-            {isShowAnswer && !isAnswerCorrect && (
-              <p className="text-red-700">
-                Correct answer is{" "}
-                <span className="font-bold underline">
-                  {getCorrectAnswer()}
-                </span>
-              </p>
-            )}
+            <QuestionsOptions
+              questions={options}
+              onSelect={handleSubmit}
+              selectedAnswer={selectedAnswer}
+              isAnswerCorrect={isAnswerCorrect}
+              questionType={questionType}
+            />
+            <Message
+              isError={isError}
+              isShowAnswer={isShowAnswer}
+              isAnswerCorrect={isAnswerCorrect}
+            />
           </div>
           <div className="flex items-center justify-end px-8">
-            <input
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-blue-200"
-              type="submit"
-              value="Submit"
-              disabled={isShowAnswer}
-            />
-            {isShowAnswer && (
-              <input
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2 w-20"
-                value="Next"
+            {isAnswerCorrect && (
+              <button
+                className="bg-blue-500 text-white font-bold py-2 px-4 rounded mx-2 w-20 hover:bg-blue-800"
                 onClick={handleNextClick}
-              />
+              >
+                Next
+              </button>
             )}
           </div>
-        </form>
+        </>
       </div>
     </Card>
   );
