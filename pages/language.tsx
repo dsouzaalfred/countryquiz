b4a1c849e2e7cf6b8f56d2d462e1af252746cfc1
Alfred Dsouza <dsouzaalfred@gmail.com>
2022-02-08 13:58:05 +0000

    add place holder pages

---
+++ pages/language.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+
+import { PageLayout } from "@/layouts/Page";
+
+const Language: NextPage = () => {
+  return (
+    <PageLayout>
+      <div className="w-full flex items-center justify-center mb-4">
+        <h2>Coming Soon</h2>
+      </div>
+    </PageLayout>
+  );
+};
+
+export default Language;
