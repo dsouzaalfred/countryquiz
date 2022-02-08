a434f35a12cd7e981377c004380d4accd892a9e8
Alfred Dsouza <dsouzaalfred@gmail.com>
2022-02-08 09:18:02 +0000

    update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,14 +8,26 @@ const Home: NextPage = () => {
     <PageLayout>
       <div className="flex flex-col w-full">
         <div className="w-full flex items-center justify-center mb-4">
-          <Link href="/flags">
-            <a className="font-bold text-blue-600 text-2xl ">Flags</a>
+          <Link href="/flag">
+            <a className="font-bold text-blue-600 text-2xl ">Flag quiz</a>
           </Link>
           <br />
         </div>
         <div className="w-full flex items-center justify-center mb-4">
-          <Link href="/capitals">
-            <a className="font-bold text-blue-600 text-2xl ">Capitals</a>
+          <Link href="/capital">
+            <a className="font-bold text-blue-600 text-2xl ">Capital quiz</a>
+          </Link>
+          <br />
+        </div>
+        <div className="w-full flex items-center justify-center mb-4">
+          <Link href="/language">
+            <a className="font-bold text-blue-600 text-2xl ">Language quiz</a>
+          </Link>
+          <br />
+        </div>
+        <div className="w-full flex items-center justify-center mb-4">
+          <Link href="/currency">
+            <a className="font-bold text-blue-600 text-2xl ">Currency quiz</a>
           </Link>
           <br />
         </div>
