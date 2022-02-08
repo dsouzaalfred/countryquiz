fb02d9b616913740182c5605e5b9a6bc9ce36c14
Alfred Dsouza <dsouzaalfred@gmail.com>
2022-02-08 16:59:08 +0000

    use only top and bottom margin

--- components/PageLink/index.tsx
+++ components/PageLink/index.tsx
@@ -7,7 +7,7 @@ interface PageLinkProps {
 
 export const PageLink = ({ link, title }: PageLinkProps) => {
   return (
-    <div className="py-4 px-4 rounded-xl space-y-2 sm:space-y-0 border border-slate-400 m-1 w-full md:w-1/4 h-28 flex flex-row items-center justify-center shadow-md">
+    <div className="py-4 px-4 rounded-xl space-y-2 sm:space-y-0 border border-slate-400 my-1 w-full md:w-1/4 h-28 flex flex-row items-center justify-center shadow-md">
       <Link href={link}>
         <a className="font-bold text-blue-600 text-2xl text-center">{title}</a>
       </Link>
