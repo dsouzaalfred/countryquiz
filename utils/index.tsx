9fdd1e8d9947203137611df901c85f7e77bdf16f
Alfred Dsouza <dsouzaalfred@gmail.com>
2022-02-08 09:16:45 +0000

    move common util functions to util

---
+++ utils/index.tsx
@@ -0,0 +1,22 @@
+import { Options, Country } from "@/types/types";
+
+export const getRandomIndex = (length: number) =>
+  Math.floor(Math.random() * length);
+
+export const constructOptions = (
+  options: number[],
+  data: Country[]
+): Options[] => {
+  const arr = options.map((option) => {
+    const country = data[option];
+    return { label: country.name, value: country.code };
+  });
+  return arr;
+};
+
+export const shuffleArray = (array: Options[]) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+};
