70a36f8378223bbab41a2156871dca5e3ef774fc
Alfred Dsouza <dsouzaalfred@gmail.com>
2022-02-11 15:12:56 +0000

    add component to link to home site

---
+++ components/Alfred/index.tsx
@@ -0,0 +1,15 @@
+export const Alfred = () => {
+  return (
+    <>
+      <span className="text-sm md:text-md tracking-wide">By&nbsp;</span>
+      <a
+        href="https://dsouzaalf.red/"
+        className="text-sm md:text-md text-primary tracking-wide"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Alfred D&apos;Souza
+      </a>
+    </>
+  );
+};
