6b67cb0fd8d30d040de6890550352734614c79c4
Alfred Dsouza <dsouzaalfred@gmail.com>
2022-02-11 15:13:26 +0000

    add by to desktop menu

--- components/Nav/DesktopNav/index.tsx
+++ components/Nav/DesktopNav/index.tsx
@@ -1,9 +1,15 @@
 import { NavBody } from "@/components/Nav/NavBody";
+import { Alfred } from "@/components/Alfred";
 
 export const DesktopNav = () => {
   return (
-    <nav className="hidden md:flex flex-col w-64 bg-light min-h-screen">
-      <NavBody />
+    <nav className="hidden md:flex flex-col w-64 justify-between bg-light min-h-screen">
+      <div>
+        <NavBody />
+      </div>
+      <div className="p-4">
+        <Alfred />
+      </div>
     </nav>
   );
 };
